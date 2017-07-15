4f764a7013a80db9ded89c6928643d47d40536fb
Gavin McDonald <gavinmcdoh@gmail.com>
2017-07-14 21:14:42 -0400

    fixed typo

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -4,7 +4,7 @@ defmodule Issues.CLI do
   @default_count 4
 
   @moduledoc """
-  Handle the command line parsing and the dispatch to
+  Handle the command line parsing and then dispatch to
   the various functions that end up generating a
   table of the last _n_ issues in a github project
   """
